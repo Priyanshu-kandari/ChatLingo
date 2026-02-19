faa47766b72779ae27cb926a15e93b356200208b
Preyanshu <preyanshukandari@gmail.com>
2026-02-19 12:24:46 +0530

    Implement axios instance and integrate React Query for API calls

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,15 +1,24 @@
 import express from "express";
-import authRoutes from "./Routes/auth.routes.js"
-import chatRoutes from "./Routes/chat.route.js"
-import userRoutes from "./Routes/user.route.js"
-import cookieParser from "cookie-parser"
+import authRoutes from "./Routes/auth.routes.js";
+import chatRoutes from "./Routes/chat.route.js";
+import userRoutes from "./Routes/user.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
-export default app
+export default app;
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,27 +7,31 @@ import HomePage from './pages/HomePage'
 import NotificationPage from './pages/NotificationPage'
 import ChatPage from './pages/ChatPage'
 import CallPage from './pages/CallPage'
-import {Toaster, toast} from "react-hot-toast"
+import {Toaster} from "react-hot-toast"
+import { useQuery } from '@tanstack/react-query'
+import { axiosInstance } from './lib/axios'
 
 const App = () => {
+
+  const {data  } = useQuery({queryKey: ["todos"], queryFn: async() => {
+    const res = await axiosInstance.get("/auth/me");
+    return res.data
+  }, retry:false})
+
+  console.log({data})
   return (
-    <>
-    <div>App
-      <button onClick={()=>
-        toast.success("hello World")
-      }>Create a toast</button>
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/signup" element={<SignUpPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/onboarding" element={<OnboardingPage/>}/>
+        <Route path="/Notification" element={<NotificationPage/>}/>
+        <Route path="/Chat" element={<ChatPage/>}/>
+        <Route path="/Call" element={<CallPage/>}/>
+      </Routes>
+      <Toaster/>
     </div>
-    <Routes>
-      <Route path="/" element={<HomePage/>}/>
-      <Route path="/signup" element={<SignUpPage/>}/>
-      <Route path="/login" element={<LoginPage/>}/>
-      <Route path="/onboarding" element={<OnboardingPage/>}/>
-      <Route path="/Notification" element={<NotificationPage/>}/>
-      <Route path="/Chat" element={<ChatPage/>}/>
-      <Route path="/Call" element={<CallPage/>}/>
-    </Routes>
-    <Toaster/>
-    </>
   )
 }
 
